55c61d8ce9e2886a1544c9d5253e814013250b09
puspendra-myhaulstore <puspendra@myhaulstore.com>
2025-11-13 09:34:00 +0000

    Fix: use Render PORT and bind to 0.0.0.0 routing

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask on Render! 🚀"
 
+@app.route('/test')
+def test():
+    return "Hello from Flask on Render test! 🚀"
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
